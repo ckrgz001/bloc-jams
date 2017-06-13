50e07cfda6e235b91b5a02a9f102df23623ca51d
ckrgz001 <ckrgz001@outlook.com>
2017-06-13 19:23:45 -0400

    Still not working

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,17 +3,19 @@ var animatePoints = function() {
     var points = document.getElementsByClassName('point');
 
     var revealPoint = function(index) {
-        for (var i = 0; i < points.length; i++){
+for (var i = 0; i < points.length; i++){
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
         points[index].style.msTransform = "scaleX(1) translateY(0)";
         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
 
-    };
+    }
+};
+
+
+    revealPoint(index);
 }
 
-    revealPoint();
 
 
-};
 animatePoints();
